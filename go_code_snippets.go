48f486387c58f8fe82323d89a071e842110ce914
lnara002 <lakshman_kumar.narayanan@nokia.com>
2016-08-30 18:08:48 -0700

    Adding go code snippets sort, type-coversion notes

---
+++ go_code_snippets.go
@@ -0,0 +1,13 @@
+package never_use_this
+
+//Read a file line by line
+func readLine(path string) {
+	inFile, _ := os.Open(path)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
